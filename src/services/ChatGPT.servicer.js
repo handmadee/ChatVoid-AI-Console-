a5ee1213ecb0fb6af74a979f1f99879d5bda54de
Dino_datcoder38 <dinodatcoder38@gmail.com>
2024-08-03 15:56:31 +0700

    featScreen

--- src/services/ChatGPT.servicer.js
+++ src/services/ChatGPT.servicer.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { OpenAI } from 'openai'; // Kiểm tra tên import chính xác trong tài liệu của thư viện
 
 const client = new OpenAI({
-    // apiKey: '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', // Thay thế bằng API Key hợp lệ
+
 });
 
 const generateImage = async () => {
